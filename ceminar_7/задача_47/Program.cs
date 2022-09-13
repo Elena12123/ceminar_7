02cc0fba1480daeeb56b4040b63f175855ccb299
KEA <lena_konovalova@rambler.ru>
2022-09-13 14:55:27 +0300

    z47

---
+++ ceminar_7/задача_47/Program.cs
@@ -0,0 +1,31 @@
+﻿void PrintArray(double[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] FillArray(int m, int n)
+{
+    double[,] array = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+           array[i, j] = Convert.ToDouble(new Random().Next(-10, 10)) /10;
+           
+    }
+    return array;
+}
+Console.Write("Введите количество строк двумерного массива ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество стобцов двумерного массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] mas = FillArray(m, n);
+
+PrintArray(mas);
