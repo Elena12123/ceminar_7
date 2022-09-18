43671262053d67493bc58d3ef23db6e1aa547333
KEA <lena_konovalova@rambler.ru>
2022-09-18 20:35:27 +0300

    z52

---
+++ ceminar_7/задача_50/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("введите номер строки");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int y = Convert.ToInt32(Console.ReadLine());
+int [,] number = new int [5,8];
+FillArrayNumber(number);
+
+if (x > number.GetLength(0) || y > number.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {x} строки и {y} столбца равно {number[x-1,y-1]}");
+}
+
+PrintArray(number);
+
+void FillArrayNumber(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(1, 100);
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+       
+        Console.WriteLine(""); 
+    }
+}
+
+
+
+
+
+
+
---
+++ ceminar_7/задача_52/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("введите количество строк ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] number = new int[n, m];
+FillArray(number);
+
+for (int j = 0; j < number.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < number.GetLength(0); i++)
+    {
+        avarage = (avarage + number[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+
+PrintArray(number);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(1, 100);
+            }   
+        }
+}
+
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+       
+        Console.WriteLine(""); 
+    }
+}
+
